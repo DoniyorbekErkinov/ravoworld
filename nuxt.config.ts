0351a5c7016d8a1d8106645c7909ee95da3ca6fc
doniyor <erkinovdoniyor03@gmail.com>
2023-12-29 23:34:36 +0500

    npm error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,23 +7,5 @@ export default defineNuxtConfig({
       tailwindcss: {},
       autoprefixer: {},
     },
-  },
-  modules: [
-    ['@nuxtjs/google-fonts', {
-        families: {
-          Manrope: true,
-          Inter: [400, 700],
-          'Josefin+Sans': true,
-          Lato: [100, 300],
-          Raleway: {
-            wght: [100, 400],
-            ital: [100]
-          },
-          'Crimson Pro': {
-            wght: '200..900',
-            ital: '200..700',
-          }
-        }
-    }],
-  ],
+  }  
 })
